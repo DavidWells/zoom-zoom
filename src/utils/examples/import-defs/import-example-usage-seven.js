3c03e4aa9ef5d784305ef2919d461a55a5847a32
DavidWells <hello@davidwells.io>
2024-04-16 16:11:12 -0700

    add example

---
+++ src/utils/examples/import-defs/import-example-usage-seven.js
@@ -0,0 +1,8 @@
+
+/** @type {MyTypeFromNoExport} */
+const myVar = {
+  name : 'Roy',
+  description : 'abc123',
+  // age: 25,
+}
+console.log(myVar)
---
+++ src/utils/examples/import-defs/import-example-with-no-export.js
@@ -0,0 +1,7 @@
+// https://stackoverflow.com/questions/49836644/how-to-import-a-typedef-from-one-file-to-another-in-jsdoc-using-vs-code
+
+/** 
+* @typedef MyTypeFromNoExport
+* @prop {string} name  
+* @prop {string} description 
+*/
